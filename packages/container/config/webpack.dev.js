7a471dce732637a6da7e3580b3f91e562c2012d8
oleksii.polonka <olpolonka@car-fluent.com>
2025-02-25 13:12:09 +0200

    init commit

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,31 @@
+const { merge } = require('webpack-merge');
+const HTMLWebpackPlugin= require('html-webpack-plugin');
+const ModularFederation = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json')
+
+packageJson.dependency
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: 'index.html'
+    }
+  },
+  plugins: [
+    new ModularFederation({
+      name: 'container',
+      remotes: {
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js'
+      },
+      shared: packageJson.dependencies
+    }),
+    new HTMLWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+};
+
+module.exports = merge(commonConfig, devConfig);

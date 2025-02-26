18b97bf9e02bf7c7ca8fa818062278d4d1d60167
oleksii.polonka <olpolonka@car-fluent.com>
2025-02-27 00:44:38 +0200

    minor changes

--- packages/auth/src/components/Signin.js
+++ packages/auth/src/components/Signin.js
@@ -37,7 +37,6 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
   },
-
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,

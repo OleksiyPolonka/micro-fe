10d6d8584f7ea7b20587b00d967b55a9bc61df62
oleksii.polonka <olpolonka@car-fluent.com>
2025-02-26 17:53:05 +0200

    add header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Marketing from './components/Marketing';
+import Header from './components/Header';
 
 export default () => {
   return (
-    <>
+    <BrowserRouter>
+      <Header />
       <Marketing />
-    </>
+    </BrowserRouter>
   )
 }
